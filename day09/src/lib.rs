48072377178694a7f42bb050140716862f0a905d
Laszlo Korte <git@laszlokorte.de>
2022-12-09 16:37:17 +0100

    alternative implementation day09

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -24,16 +24,40 @@ struct Movement {
     distance: u32,
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Rope {
+    head: Position,
+    segments: Vec<Position>,
+}
+
 impl Movement {
     fn to_steps(self) -> Vec<Direction> {
         vec![self.direction; self.distance as usize]
     }
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-struct Position {
-    x: i32,
-    y: i32,
+impl Direction {
+    fn dx(&self) -> i32 {
+        match self {
+            Self::Left => 1,
+            Self::Right => -1,
+            _ => 0,
+        }
+    }
+
+    fn dy(&self) -> i32 {
+        match self {
+            Self::Up => -1,
+            Self::Down => 1,
+            _ => 0,
+        }
+    }
 }
 
 impl Position {
@@ -42,55 +66,39 @@ impl Position {
     }
 
     fn moved_in_direction(self, dir: Direction) -> Self {
-        match dir {
-            Direction::Up => Self {
-                y: self.y - 1,
-                ..self
-            },
-            Direction::Down => Self {
-                y: self.y + 1,
-                ..self
-            },
-            Direction::Left => Self {
-                x: self.x - 1,
-                ..self
-            },
-            Direction::Right => Self {
-                x: self.x + 1,
-                ..self
-            },
+        Self {
+            x: self.x + dir.dx(),
+            y: self.y + dir.dy(),
         }
     }
+
     fn follow(self, head: Position) -> Option<Position> {
         let delta_x = head.x - self.x;
         let delta_y = head.y - self.y;
-        let max = i32::max(delta_x.abs(), delta_y.abs());
+        let (x_abs, x_sgn) = (delta_x.abs(), delta_x.signum());
+        let (y_abs, y_sgn) = (delta_y.abs(), delta_y.signum());
+        let chebyshev_distance = i32::max(x_abs, y_abs);
 
-        if max < 2 {
+        if chebyshev_distance < 2 {
             Some(self)
         } else {
-            match (delta_x.abs(), delta_y.abs()) {
+            match (x_abs, y_abs) {
                 (_, 0) => Some(Position {
-                    x: self.x + delta_x.signum(),
+                    x: self.x + x_sgn,
                     ..self
                 }),
                 (0, _) => Some(Position {
-                    y: self.y + delta_y.signum(),
+                    y: self.y + y_sgn,
                     ..self
                 }),
                 (l, r) if l + r <= 4 => Some(Position {
-                    x: self.x + delta_x.signum(),
-                    y: self.y + delta_y.signum(),
+                    x: self.x + x_sgn,
+                    y: self.y + y_sgn,
                 }),
                 _ => None,
             }
         }
     }
-}
-
-struct Rope {
-    head: Position,
-    segments: Vec<Position>,
 }
 
 impl Rope {
@@ -101,7 +109,7 @@ impl Rope {
         }
     }
 
-    fn move_head(self, dir: Direction) -> Option<Rope> {
+    fn move_head(&self, dir: Direction) -> Option<Rope> {
         let new_head = self.head.moved_in_direction(dir);
 
         Some(Rope {
@@ -151,7 +159,6 @@ pub fn process(input: String, length: usize) -> Option<usize> {
     let (_, mvs) = moves(&input).ok()?;
 
     let mut rope = Rope::new(length);
-
     let mut visited = BTreeSet::<Position>::new();
 
     for dir in mvs.iter().copied().flat_map(Movement::to_steps) {
@@ -161,6 +168,27 @@ pub fn process(input: String, length: usize) -> Option<usize> {
     }
 
     Some(visited.len())
+}
+
+pub fn process_alternative(input: String, length: usize) -> Option<usize> {
+    let (_, mvs) = moves(&input).ok()?;
+
+    Some(
+        mvs.iter()
+            .copied()
+            .flat_map(Movement::to_steps)
+            .scan(Some(Rope::new(length)), |r, dir| {
+                if let Some(ref rr) = r {
+                    let rrr = rr.move_head(dir)?;
+                    *r = Some(rrr.clone());
+                    Some(rrr.tail())
+                } else {
+                    None
+                }
+            })
+            .collect::<Option<BTreeSet<Position>>>()?
+            .len(),
+    )
 }
 
 #[cfg(test)]
@@ -180,6 +208,9 @@ mod tests {
 
         assert_eq!(process(COMMANDS.to_string(), 1), Some(13));
         assert_eq!(process(COMMANDS.to_string(), 9), Some(1));
+
+        assert_eq!(process_alternative(COMMANDS.to_string(), 1), Some(13));
+        assert_eq!(process_alternative(COMMANDS.to_string(), 9), Some(1));
     }
 
     #[test]
@@ -194,5 +225,6 @@ mod tests {
         U 20";
 
         assert_eq!(process(COMMANDS.to_string(), 9), Some(36));
+        assert_eq!(process_alternative(COMMANDS.to_string(), 9), Some(36));
     }
 }
