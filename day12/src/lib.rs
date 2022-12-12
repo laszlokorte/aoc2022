1da7790c3760dcf0daba201b371d28f862b1cb80
Laszlo Korte <git@laszlokorte.de>
2022-12-12 19:36:23 +0100

    further cleanup

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(iter_array_chunks)]
 #![feature(iter_intersperse)]
+#![feature(exclusive_range_pattern)]
 
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -17,9 +18,39 @@ struct Field {
     marker: Option<Marker>,
 }
 
-impl From<char> for Field {
-    fn from(value: char) -> Self {
-        match value {
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Hash)]
+struct GridPosition {
+    row: usize,
+    column: usize,
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+struct Step {
+    dx: i32,
+    dy: i32,
+}
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub struct Policy {
+    moves: [Step; 4],
+    max_up: Option<i32>,
+    min_up: Option<i32>,
+}
+
+pub enum SearchTerm {
+    Height(u32),
+    Marker(Marker),
+}
+
+#[derive(Debug)]
+struct Grid<'a> {
+    fields: &'a Vec<Vec<Field>>,
+}
+
+impl TryFrom<char> for Field {
+    type Error = String;
+
+    fn try_from(value: char) -> Result<Self, String> {
+        Ok(match value {
             'S' => Self {
                 height: 0,
                 marker: Some(Marker::Start),
@@ -28,30 +59,13 @@ impl From<char> for Field {
                 height: 25,
                 marker: Some(Marker::End),
             },
-            _ => Self {
+            'a'..='z' => Self {
                 height: value as u32 - 'a' as u32,
                 marker: None,
             },
-        }
+            c => return Err(format!("Invalid character: '{c}', expect a..z or S or E")),
+        })
     }
-}
-
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
-struct Step {
-    dx: i32,
-    dy: i32,
-}
-
-impl From<(i32, i32)> for Step {
-    fn from((dx, dy): (i32, i32)) -> Self {
-        Step { dx, dy }
-    }
-}
-
-pub struct Policy {
-    moves: [Step; 4],
-    max_up: Option<i32>,
-    min_up: Option<i32>,
 }
 
 impl Policy {
@@ -76,7 +90,7 @@ impl Policy {
         min_up: Some(-1),
     };
 
-    fn allows(&self, from: Field, to: Field) -> bool {
+    fn allows(&self, from: &Field, to: &Field) -> bool {
         let height_difference = to.height as i32 - from.height as i32;
         match (self.min_up, self.max_up) {
             (Some(u), Some(d)) => (d..=u).contains(&height_difference),
@@ -85,11 +99,6 @@ impl Policy {
             _ => true,
         }
     }
-}
-
-pub enum SearchTerm {
-    Height(u32),
-    Marker(Marker),
 }
 
 impl SearchTerm {
@@ -101,17 +110,12 @@ impl SearchTerm {
     }
 }
 
-#[derive(Debug)]
-struct Grid<'a> {
-    fields: &'a Vec<Vec<Field>>,
-}
-
 impl<'a> Grid<'a> {
-    fn find_marker(&self, marker: Marker) -> Option<(usize, usize)> {
+    fn find_marker(&self, marker: Marker) -> Option<GridPosition> {
         self.fields.iter().enumerate().find_map(|(r, row)| {
             row.iter().enumerate().find_map(|(c, col)| {
                 if col.marker == Some(marker) {
-                    Some((r, c))
+                    Some(GridPosition { row: r, column: c })
                 } else {
                     None
                 }
@@ -121,35 +125,38 @@ impl<'a> Grid<'a> {
     fn neighbour_positions(
         &'a self,
         policy: &'a Policy,
-        pos: &'a (usize, usize),
-    ) -> impl Iterator<Item = (usize, usize)> + 'a {
+        pos: &'a GridPosition,
+    ) -> impl Iterator<Item = GridPosition> + 'a {
         policy
             .moves
             .iter()
-            .flat_map(move |s| self.relative_position(&policy, pos, s))
+            .flat_map(move |s| self.relative_position(policy, pos, s))
     }
 
     fn relative_position(
         &self,
         policy: &Policy,
-        pos: &(usize, usize),
+        pos: &GridPosition,
         step: &Step,
-    ) -> Option<(usize, usize)> {
-        let new_column = (pos.1 as i32 + step.dx) as usize;
-        let new_row = (pos.0 as i32 + step.dy) as usize;
+    ) -> Option<GridPosition> {
+        let new_column = (pos.column as i32 + step.dx) as usize;
+        let new_row = (pos.row as i32 + step.dy) as usize;
 
         if new_row < self.fields.len()
             && new_column < self.fields[new_row].len()
-            && policy.allows(self.fields[pos.0][pos.1], self.fields[new_row][new_column])
+            && policy.allows(self.field_at(pos)?, &self.fields[new_row][new_column])
         {
-            Some((new_row, new_column))
+            Some(GridPosition {
+                row: new_row,
+                column: new_column,
+            })
         } else {
             None
         }
     }
 
-    fn field_at(&self, (row, col): (usize, usize)) -> Option<&Field> {
-        self.fields.get(row)?.get(col)
+    fn field_at(&self, position: &GridPosition) -> Option<&Field> {
+        self.fields.get(position.row)?.get(position.column)
     }
 }
 
@@ -161,18 +168,21 @@ pub fn process(
 ) -> Option<u32> {
     let cells = input
         .lines()
-        .map(|l| l.chars().map(Field::from).collect())
-        .collect::<Vec<Vec<_>>>();
+        .map(|l| l.chars().map(Field::try_from).map(Result::ok).collect())
+        .collect::<Option<Vec<Vec<_>>>>()?;
     let grid = Grid { fields: &cells };
     let start = grid.find_marker(start)?;
+
     let mut seen = HashSet::new();
-    let mut predecessor = HashMap::<(usize, usize), (usize, usize)>::new();
-    let mut queue = VecDeque::<(usize, usize)>::new();
+    let mut predecessor = HashMap::<GridPosition, GridPosition>::new();
+    let mut queue = VecDeque::<GridPosition>::new();
+
     queue.push_back(start);
     seen.insert(start);
+
     let goal = 'find: {
         while let Some(field) = queue.pop_front() {
-            if search_term.matches(grid.field_at(field)?) {
+            if search_term.matches(grid.field_at(&field)?) {
                 break 'find field;
             }
             for n in grid.neighbour_positions(&policy, &field) {
@@ -187,7 +197,7 @@ pub fn process(
 
     let mut count = 0;
     let mut current = &goal;
-    while let Some(pred) = &predecessor.get(&current) {
+    while let Some(pred) = &predecessor.get(current) {
         count += 1;
         current = *pred;
     }
